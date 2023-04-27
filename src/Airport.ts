b8b876fcdfdb4aab63c49acaecd954cb8359b52e
NONG.PHLOEUT <NONG.PHLOEUT@PNC.LAN>
2023-04-27 13:08:24 +0700

    write method in class ariport

--- src/Airport.ts
+++ src/Airport.ts
@@ -3,8 +3,13 @@
 export class Airport {
     name:string;
     address:string;
+
+    // public services 
+    // public booking:booking = new booking()
     constructor(name:string,address:string) {
         this.name = name;
         this.address = address;
     }
+
+
 }
